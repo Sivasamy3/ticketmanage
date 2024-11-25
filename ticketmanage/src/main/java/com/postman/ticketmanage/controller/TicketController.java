26f6547297ac91a31879158ec45903f9fa853ffa
Sivasamy3 <sivasamyeswaran303@gmail.com>
2024-11-25 09:30:37 +0530

    second commit

--- ticketmanage/src/main/java/com/postman/ticketmanage/controller/TicketController.java
+++ ticketmanage/src/main/java/com/postman/ticketmanage/controller/TicketController.java
@@ -1,9 +1,15 @@
 package com.postman.ticketmanage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.postman.ticketmanage.model.UserReq;
 import com.postman.ticketmanage.service.TicketService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TicketController {
@@ -21,6 +27,6 @@ public class TicketController {
     @PostMapping("/register")
     public String register(@RequestBody UserReq userreq){
         System.out.println(userreq);
-        return userreq.getName()+" you are successfully registered";
+        return userreq.getName()+" you are successfully registered ";
     }
 }
